059d2498142f45c5da68dd7b5111bdcf17c48cef
IvanMontejano <ivan.montejano@gmail.com>
2026-03-12 12:30:51 +0100

    Initial commit

---
+++ src/test/java/com/ivanmontejano/Criaturas/CriaturasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivanmontejano.Criaturas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CriaturasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
